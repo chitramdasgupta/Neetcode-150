d6ef6bfa80d3d5f1c7091d2c442ba451c8657cc4
Chitram Dasgupta <chitramdasgupta@gmail.com>
2023-11-18 22:07:21 +0530

    Solved 3 medium problems from heaps

---
+++ src/HeapOrPriorityQueue/TaskScheduler.java
@@ -0,0 +1,39 @@
+package HeapOrPriorityQueue;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+// Time - O(n)
+// Space - O(n)
+public class TaskScheduler {
+    public int leastInterval(char[] tasks, int n) {
+        if (n == 0) {
+            return tasks.length;
+        }
+        // Will hold the frequency of each task and poll the most frequent one
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+
+        // The remaining freq of each task along with the time it will again be available
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+
+        int[] arr = new int[26];
+        for (char c : tasks) arr[c - 'A']++;
+        for (int val : arr) if (val > 0) pq.add(val);
+
+        int time = 0;
+        while ((!pq.isEmpty() || !q.isEmpty())) {
+            time++;
+            if (!pq.isEmpty()) {
+                int val = pq.poll();
+                val--;
+                if (val > 0) q.add(new Pair(val, time + n));
+            }
+
+            if (!q.isEmpty() && q.peek().getValue() == time) pq.add(
+                    q.poll().getKey()
+            );
+        }
+        return time;
+    }
+}
